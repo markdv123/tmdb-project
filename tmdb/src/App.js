1ff5a3ad65ecf37c7c999f6b119d0526b7dd87c9
Mark DeVito <markdv@alumni.unc.edu>
2021-02-04 13:30:37 -0500

    dotenv and axios setup, useEffect gets list of popular movies onmount

--- tmdb/src/App.js
+++ tmdb/src/App.js
@@ -2,7 +2,19 @@ import './styles/App.css'
 import { ThemeProvider } from '@material-ui/core/styles'
 import theme from './styles/Theme'
 import React, { useState, useEffect } from 'react'
+import Axios from 'axios'
+const API_KEY = process.env.REACT_APP_API_KEY
+
 function App() {
+  const [movies, setMovies] = useState([])
+
+  getMovies = async () => {
+    res = await Axios.get(`https://api.themoviedb.org/3/trending/movie/day?api_key=${API_KEY}`)
+  }
+
+  useEffect(()=> {
+    getMovies()
+  }, [])
 
   return (
     <ThemeProvider theme={theme}>
